7edaee5d89529921d4aa251f23d244f2a9496434
Chris Mems <christopher.mems@.com>
2022-09-21 11:41:21 -0500

    fix views/skills/index.ejs structure

--- data/skills-data.js
+++ data/skills-data.js
@@ -2,8 +2,9 @@ const skills = [
   {text: 'JavaSript', experience: true, _id: 125223},
   {text: 'Git', experience: true, _id: 127904},
   {text: 'HTML', experience: true, _id: 139608},
-  {text: 'CSS', experience: true, _id: 121212}
-  {text: 'Java', experience: false, _id: 131313}
+  {text: 'CSS', experience: true, _id: 121212},
+  {text: 'Java', experience: false, _id: 131313},
+  {text: 'C++', experience: false, _id: 159340},
 ]
 
 export { 
