bcb61bc26cbdef1c315dba39787d4a2cd8b7450a
Chris Mems <christopher.mems@.com>
2022-09-21 07:25:08 -0500

    add skills

--- data/skills-data.js
+++ data/skills-data.js
@@ -1,7 +1,9 @@
 const skills = [
-  {text: 'Feed llama', done: true, _id: 125223},
-  {text: 'Sleep under the stars', done: false, _id: 127904},
-  {text: 'Buy milk', done: false, _id: 139608},
+  {text: 'JavaSript', experience: true, _id: 125223},
+  {text: 'Git', experience: true, _id: 127904},
+  {text: 'HTML', experience: true, _id: 139608},
+  {text: 'CSS', experience: true, _id: 121212}
+  {text: 'Java', experience: false, _id: 131313}
 ]
 
 export { 
